53505237f945bd7ecfb39ce29ea8ab21eb99af2b
Tima <80810016+Tima99@users.noreply.github.com>
2024-03-11 20:57:38 +0530

    added aws s3

--- services/uploadImage.js
+++ services/uploadImage.js
@@ -4,10 +4,22 @@ const {
     adjectives,
     colors,
 } = require("unique-names-generator");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { Readable } = require("stream");
+const { Upload } = require("@aws-sdk/lib-storage");
 
 const uploadDir = path.join(__dirname, "../uploads");
 
-const uploadImage = async (image, cutsom_adjectives = []) => {
+const s3Client = new S3Client({
+    region: process.env.S3_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+    },
+});
+const s3BucketName = process.env.S3_BUCKET_NAME;
+
+const uploadImage = async (image, custom_adjectives = []) => {
     if (image && !/^image/.test(image.mimetype))
         throw new Error("Bad Image", {
             cause: {
@@ -16,14 +28,40 @@ const uploadImage = async (image, cutsom_adjectives = []) => {
         });
 
     const uniqueName = uniqueNamesGenerator({
-        dictionaries: [cutsom_adjectives, adjectives, colors],
+        dictionaries: [custom_adjectives, adjectives, colors],
         separator: "-",
     });
-    const imageExt = image && image.name.split(".").at(-1);
+    const imageExt = image && image.name.split(".").pop();
     const uploadImageName = `${uniqueName}.${imageExt}`;
-    image && (await image.mv(path.join(uploadDir, uploadImageName)));
 
-    return uploadImageName;
+    // Convert the file buffer to a stream
+    const stream = Readable.from(image.data);
+
+    // const params = {
+    //     Bucket: s3BucketName,
+    //     Key: uploadImageName,
+    //     Body: stream,
+    // };
+
+    const upload = new Upload({
+        client: s3Client,
+        params: {
+            Bucket: s3BucketName,
+            Key: uploadImageName,
+            Body: stream,
+        },
+    });
+
+    
+    try {
+        // await s3Client.send(new PutObjectCommand(params));
+        await upload.done();
+
+        return uploadImageName;
+    } catch (err) {
+        console.error("Error uploading to S3:", err);
+        throw err;
+    }
 };
 
 module.exports = uploadImage;
